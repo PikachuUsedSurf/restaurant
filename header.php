2ecd11e92854f31778ec729609134ce334601dd1
PikachuUsedSurf <nanaosafokissi@gmail.com>
2024-03-24 15:14:49 +0300

    added header as inlude

---
+++ header.php
@@ -0,0 +1,12 @@
+<nav class="navbar">
+        <div class="container">
+            <a href="index.html" class="logo">GOURMET HAVEN</a>
+            <ul class="nav-links">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="menu.html">Menu</a></li>
+                <li><a href="reservation.html">Reservations</a></li>
+                <li><a href="checkout.html">Checkout</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </div>
+</nav>
--- login.php
+++ login.php
@@ -63,18 +63,9 @@ $conn->close();
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <nav class="navbar">
-        <div class="container">
-            <a href="index.html" class="logo">GOURMET HAVEN</a>
-            <ul class="nav-links">
-                <li><a href="index.html">Home</a></li>
-                <li><a href="menu.html">Menu</a></li>
-                <li><a href="reservation.html">Reservations</a></li>
-                <li><a href="checkout.html">Checkout</a></li>
-                <li><a href="contact.html">Contact</a></li>
-            </ul>
-        </div>
-    </nav>
+<?php
+include('header.php')
+?>
     <header>
         <h1>LOGIN</h1>
         <img src="./public/images/restaurant_logo.jpg" alt="Gourmet Haven Logo" class="curvy-shadow">
