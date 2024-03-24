05b911c649b68ef5a3c059398558ba929b6e995a
PikachuUsedSurf <nanaosafokissi@gmail.com>
2024-03-24 15:38:01 +0300

    changed header

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <nav class="navbar">
         <div class="container">
-            <a href="index.html" class="logo">GOURMET HAVEN</a>
+            <a href="index.php" class="logo">GOURMET HAVEN</a>
             <ul class="nav-links">
-                <li><a href="index.html">Home</a></li>
-                <li><a href="menu.html">Menu</a></li>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="menu.php">Menu</a></li>
                 <li><a href="reservation.php">Reservations</a></li>
-                <li><a href="checkout.html">Checkout</a></li>
-                <li><a href="contact.html">Contact</a></li>
+                <li><a href="checkout.php">Checkout</a></li>
+                <li><a href="contact.php">Contact</a></li>
                 <li class="signup-login"><a href="signup.php">Signup</a></li>
         <li class="signup-login"><a href="login.php">Login</a></li>
             </ul>
